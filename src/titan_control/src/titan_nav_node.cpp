f04034952d9e45903cd000ad30e0d49ab0609366
Anthony Magner <anthony.magner@gmail.com>
2017-10-13 13:45:28 -0600

    nav

--- src/titan_control/src/titan_nav_node.cpp
+++ src/titan_control/src/titan_nav_node.cpp
@@ -2,6 +2,8 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include "std_srvs/Empty.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <stdlib.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -11,12 +13,44 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "titan_nav_node");
 	ros::NodeHandle nh;
 
+	ros::Publisher initialpose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1000);
+	geometry_msgs::PoseWithCovarianceStamped poseStart;
+
+	ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("test", 1000);
+	std_msgs::String msg;		
+	msg.data = "test";
+	chatter_pub.publish(msg);
+
 	ros::ServiceClient client = nh.serviceClient<std_srvs::Empty>("/move_base/clear_costmap");
 	std_srvs::Empty srvClearCostMap;
 
+	char frame_id[] = "map";
+	poseStart.header.frame_id = frame_id;
+	poseStart.header.stamp=ros::Time::now();
+	poseStart.header.seq = 0;
+
+	poseStart.pose.covariance[0] = 0.25;
+	poseStart.pose.covariance[7] = 0.25;
+	poseStart.pose.covariance[35] = 0.1;
+
+	poseStart.pose.pose.position.x = 0.0;
+	poseStart.pose.pose.position.y = 0.0;
+	poseStart.pose.pose.position.z = 0.0;
+
+	poseStart.pose.pose.orientation.x = 0.0;
+        poseStart.pose.pose.orientation.y = 0.0;
+        poseStart.pose.pose.orientation.z = 0.0;
+	poseStart.pose.pose.orientation.w = 1.0;
+
+	initialpose_pub.publish(poseStart);
+
+	ROS_INFO("Initial pose published!");
+	ros::spinOnce();
 
 
-	MoveBaseClient ac("move_base", true);
+
+
+	/*MoveBaseClient ac("move_base", true);
         while (!ac.waitForServer(ros::Duration(5.0)))
         {
                 ROS_INFO("Waiting for movebase server");
@@ -30,7 +64,7 @@ int main(int argc, char **argv)
 	{
 		ROS_ERROR("Failed to call service /move_base/clear_costmap");
 		return 1;
-	}
+	}*/
 
 	
 
